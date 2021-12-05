2d43f2374dfe57544450e012ff3445c07a391154
7ua <80481493+7ua@users.noreply.github.com>
2021-12-05 04:55:39 -0800

    Update placeholder.c

--- scripts/placeholder.c
+++ scripts/placeholder.c
@@ -1,1 +1,7 @@
+/* finish */
 
+
+/* 
+ If you came here for the scripts, ok i guess but they are not fully out yet they are very buggy and if i release now they will will probally get patched, sorry.
+ although you can implement them yourself with the list of the apis on the readme.md 
+ */
