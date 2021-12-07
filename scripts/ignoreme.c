e248b76455bd8f9222020ada0930fc8b1f7f9073
7ua <80481493+7ua@users.noreply.github.com>
2021-12-06 18:39:18 -0800

    Delete ignoreme.c

--- scripts/ignoreme.c
+++
@@ -1,6 +0,0 @@
-
-/* 
- If you came here for the scripts, ok i guess but they are not fully out yet they are very buggy and if i release now they will will probally get patched, sorry.
- although you can implement them yourself with the list of the apis on the readme.md, i'll try releasing this in javascript instead and you would be able to run it
- in the chrome browser or as a bookmarklet.
- */
